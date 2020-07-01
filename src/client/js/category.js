a2ce66b2ce51d3545e59909842dda0e804ba3182
tokkk <toqipopo@outlook.com>
2020-07-01 11:09:42 +0800

    再次整理思路

--- src/client/js/category.js
+++ src/client/js/category.js
@@ -7,7 +7,7 @@ $(() => {
           return `<li class="list" data-id=${item.good_id}>
                 <div class="p-cont">
                   <div class="p_img">
-                    <a href=""><img src="${item.src}" alt=""></a>
+                    <a href=""><img src="${item.src}" alt="" title="${item.info}"></a>
                     <div class="p-buy">立即购买</div>
                   </div>
                   <div class="p-tags"></div>
@@ -30,22 +30,36 @@ $(() => {
       $(this).siblings().find(".p-buy").removeClass("p-buyon");
     //   console.log("aa")
   })
+  $(".cata-prod").on("mouseleave",".list",function(){
+    $(this).find(".p-buy").removeClass("p-buyon");
+  //   console.log("aa")
+})
+
+//   点击立即购买
+
   $(".cata-prod").on("click",".p-buy",function(){
     // console.log("aa")
     let user_id = localStorage.getItem("user_id") || "";
     let user_name = localStorage.getItem("user_name") || "";
-    let good_id = $(this).parent().attr("data-id");
-
+    let good_id = $(this).parents(".list").attr("data-id");
+    // console.log(user_id, user_name,good_id);
     if(user_id && user_name){
         $.ajax({
             url: "../../server/addCar.php",
-            data: {user_id,good_id},
+            data: { user_id, good_id },
             success: function (data) {
-                
+                alert(data);
             }
         });
+    }else{
+        location.href = "./login.html";
     }
 
+  })
+
+//   跳转到购物车
+  $(".cart").click(function(){
+      open("./cart.html");
   })
 
 
